02d2c7da52146ab3ab57431643871958be5159f8
akrimahuzaifa123 <akrimaamirka@gmail.com>
2021-05-30 03:03:22 +0500

    app is updated with 2nd heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <h1>This code is being pushed to gitHub</h1>
+      <h1>This code is being pushed to gitHub 1</h1>
+      <h1>This code is being pushed to gitHub 2</h1>
     </div>
   );
 }
